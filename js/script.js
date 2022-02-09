b0786d604b83550ae0a3139c76264e8047ff35d7
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-02-09 17:10:25 -0300

    versão com a barra aside funcionando

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+function checkAside() {
+    const aside = document.querySelector("aside");
+
+    aside.classList.remove("hidden");
+}
+
+function uncheckAside() {
+    const aside = document.querySelector("aside");
+
+    aside.classList.add("hidden");
+}
